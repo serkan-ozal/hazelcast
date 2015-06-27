6c27a185db737288ac1847d83bcf89794336d7ba
Serkan OZAL <serkanozal@Serkans-MacBook-Pro.local>
2015-06-27 21:00:59 +0300

    Dispose data in "MessageFlyweight" after writing it to buffer

--- hazelcast/src/main/java/com/hazelcast/client/impl/protocol/util/MessageFlyweight.java
+++ hazelcast/src/main/java/com/hazelcast/client/impl/protocol/util/MessageFlyweight.java
@@ -19,6 +19,7 @@ package com.hazelcast.client.impl.protocol.util;
 import com.hazelcast.nio.Bits;
 import com.hazelcast.nio.serialization.Data;
 import com.hazelcast.nio.serialization.DefaultData;
+import com.hazelcast.nio.serialization.DisposableData;
 import com.hazelcast.nio.serialization.SelfWritableData;
 import com.hazelcast.nio.serialization.impl.DefaultData;
 
@@ -106,6 +107,9 @@ public class MessageFlyweight {
         } else {
             final byte[] bytes = data.toByteArray();
             set(bytes);
+            if (data instanceof DisposableData) {
+                ((DisposableData) data).dispose();
+            }
         }
         return this;
     }
@@ -115,6 +119,9 @@ public class MessageFlyweight {
         set(length);
         buffer.putSelfWritableData(index, selfWritableData);
         index += length;
+        if (selfWritableData instanceof DisposableData) {
+            ((DisposableData) selfWritableData).dispose();
+        }
         return this;
     }
 
